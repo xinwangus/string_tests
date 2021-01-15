eb1d9616eb152eeb265a6f7d0abbf1204c7d6c1f
Xin Wang <xin@Xins-MacBook-Air.local>
2021-01-15 11:10:48 -0500

    Changes to be committed: 	deleted:    revert_sentence_2.cc 	modified:   string_permutation_2.cc

--- string_permutation_2.cc
+++ string_permutation_2.cc
@@ -3,6 +3,7 @@
 // permutations with duplicates allowed 
 // from a string
 // Much cleaner than the first version
+// swap-recursive-backtrack
 //
 #include <stdlib.h> 
 #include <string>
@@ -10,19 +11,17 @@
 
 using namespace std; 
 
-void permute(string a, int l, int r) 
+void permute(string& a, int l, int r) 
 { 
-	// cout << "debug " << a << " " << l << " " << r << endl;
 	if (l == r) {
 		cout << a << endl;  // could save off as well
+                                    // this prints out the whole string
 	} else { 
 		for (int i = l; i <= r; i++) 
 		{ 
 			swap(a[l], a[i]); 
-
 			// recursion
 			permute(a, l+1, r); 
-
 			//backtrack, important!
 			swap(a[l], a[i]); 
 		} 
